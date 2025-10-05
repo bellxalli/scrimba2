4c1b07a4a6db4724f9edac3aa116af438c011d77
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-10-05 16:33:59 -0700

    added comment to download image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 export default function Header() {
     return (
         <header>
+            {/* get image in a moment... */}
             <img src="../globe.png" alt="globe icon" />
             <h1>my travel journal.</h1>
         </header>
