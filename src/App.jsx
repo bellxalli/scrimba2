8418d9fcd9fcf514a7d7d71b40f09672699d1387
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-10-05 22:35:43 -0700

    completed travel page aand hopefully git hub pages renders it

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,8 @@ export default function App()
     {
         return (
             <Entry
-                img={entry.img}
-                title={entry.title}
-                country={entry.country}
-                googleMapsLink={entry.googleMapsLink}
-                dates={entry.dates}
-                text={entry.text}
+                key={entry.id}
+                {...entry}
             />
         )
     })
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header()
     return (
         <header>
             {/* get image in a moment... */}
-            <img src="../globe.png" alt="globe icon" />
+            <img src="../images/globe.jpg" alt="globe icon" />
             <h1>my travel journal.</h1>
         </header>
     )
