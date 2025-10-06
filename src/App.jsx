053aead35e8dc4d18203520cfb31cf8622269741
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-10-05 20:01:38 -0700

    made edits to app.jsx and working on fixing rendering issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import Header from "./components/header"
+import Entry from "./components/Entry"
+import data from "./data"
 
-export default function App() {
+export default function App() 
+{
+    
+    const entryElements = data.map((entry) => 
+    {
+        return (
+            <Entry
+                img={entry.img}
+                title={entry.title}
+                country={entry.country}
+                googleMapsLink={entry.googleMapsLink}
+                dates={entry.dates}
+                text={entry.text}
+            />
+        )
+    })
+    
     return (
-        <Header />
+        <>
+            <Header />
+            <main className="container">
+                {entryElements}
+            </main>
+        </>
     )
 }
